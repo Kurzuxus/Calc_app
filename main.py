bb807c482a2fdc61d967357a9de6495d13becc0c
Sid <128059736+Kurzuxus@users.noreply.github.com>
2026-02-05 22:46:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ class Main_view(ft.View):
                         helper_text=cof,
                         suffix_text='/20',
                         label='Exam',
-                        width=100,
+                        width=150,
                         helper_style=ft.TextStyle(
                             color='black',
                             font_family='Century Gothic'),
@@ -155,7 +155,7 @@ class Main_view(ft.View):
                             size=20)
                     ),
                     bgcolor='#F5EBFD',
-                    width=350,
+                    width=450,
                     title_alignment=ft.ListTileTitleAlignment.CENTER
                 )                
             return Tilelist
@@ -329,6 +329,7 @@ def main(page: ft.Page):
 
 
 ft.app(main,assets_dir='assets')
+
 
 
 
